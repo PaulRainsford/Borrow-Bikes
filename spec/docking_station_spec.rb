86af8406432a921f1f06b7b51c5208e28fd07fc9
Paul Rainsford <paulrainsford69@gmail.com>
2021-10-27 21:43:45 +0100

    Removed unnecessary tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,14 +13,14 @@ describe DockingStation do
       end
     end
 
-    it { is_expected.to respond_to(:dock).with(1).argument }
+    #it { is_expected.to respond_to(:dock).with(1).argument }
 
-    it { is_expected.to respond_to(:bike) }
+    #it { is_expected.to respond_to(:bike) }
 
-    it 'docks something' do
-        bike = Bike.new
-        expect(subject.dock(bike)).to eq bike
-    end
+   #it 'docks something' do
+       # bike = Bike.new
+        #expect(subject.dock(bike)).to eq bike
+    #end
 
     it 'returns docked bikes' do
         bike = Bike.new
@@ -28,12 +28,12 @@ describe DockingStation do
         expect(subject.bike).to eq bike
     end
 
-    describe '#release_bike' do
-      it 'releases a bike' do
-        bike = Bike.new
-        subject.dock(bike)
-        expect(subject.release_bike).to eq bike
-      end
-    end
+    #describe '#release_bike' do
+      #it 'releases a bike' do
+        #bike = Bike.new
+        #subject.dock(bike)
+        #expect(subject.release_bike).to eq bike
+      #end
+    #end
 
 end
