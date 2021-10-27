032dce263a17d56391c85a64487332a12e7205de
Paul Rainsford <paulrainsford69@gmail.com>
2021-10-27 21:59:38 +0100

    Added guard condition if station full and passing test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,27 +13,15 @@ describe DockingStation do
       end
     end
 
-    #it { is_expected.to respond_to(:dock).with(1).argument }
-
-    #it { is_expected.to respond_to(:bike) }
-
-   #it 'docks something' do
-       # bike = Bike.new
-        #expect(subject.dock(bike)).to eq bike
-    #end
-
-    it 'returns docked bikes' do
+    describe '#dock' do
+      it 'returns docked bikes' do
         bike = Bike.new
         subject.dock(bike)
         expect(subject.bike).to eq bike
+      end
+      it 'raises an error when full' do
+        subject.dock(Bike.new)
+        expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+      end
     end
-
-    #describe '#release_bike' do
-      #it 'releases a bike' do
-        #bike = Bike.new
-        #subject.dock(bike)
-        #expect(subject.release_bike).to eq bike
-      #end
-    #end
-
 end
