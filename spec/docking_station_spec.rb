5d9c3e60ea707bf5d672e247f6a8b7778666df6f
Paul Rainsford <paulrainsford69@gmail.com>
2021-10-27 22:25:34 +0100

    Added bikes array, station capacity and passing test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,14 +13,10 @@ describe DockingStation do
       end
     end
 
+
     describe '#dock' do
-      it 'returns docked bikes' do
-        bike = Bike.new
-        subject.dock(bike)
-        expect(subject.bike).to eq bike
-      end
       it 'raises an error when full' do
-        subject.dock(Bike.new)
+        20.times { subject.dock Bike.new }
         expect { subject.dock Bike.new }.to raise_error 'Docking station full'
       end
     end
