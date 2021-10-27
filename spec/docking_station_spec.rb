8b3a25eb1c1ba4e854a07a772c3e786f7c8b8336
Paul Rainsford <paulrainsford69@gmail.com>
2021-10-27 21:34:10 +0100

    Added guard condition if no bikes available and passing test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,8 +8,11 @@ describe DockingStation do
         subject.dock(bike)
         expect(subject.release_bike).to eq bike
       end
+      it 'raises an error when there are no bikes available' do
+        expect { subject.release_bike }.to raise_error 'No bikes available'
+      end
     end
-    
+
     it { is_expected.to respond_to(:dock).with(1).argument }
 
     it { is_expected.to respond_to(:bike) }
