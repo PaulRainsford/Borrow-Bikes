d9bc54f29d3dde2bbd33239f2ec3162285ef2108
Paul Rainsford <paulrainsford69@gmail.com>
2021-10-28 19:41:54 +0100

    Added a private attr_reader for bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,28 +3,33 @@ require_relative 'bike'
 class DockingStation
     DEFAULT_CAPACITY = 20
 
+    attr_reader :capacity
+
     def initialize
         @bikes = []
+        @capacity = DEFAULT_CAPACITY
     end
 
     def dock(bike)
-        fail 'Docking station full' if full?
-      @bikes << bike
+      fail 'Docking station full' if full?
+      bikes << bike
     end
 
     def release_bike
         fail 'No bikes available' if empty? 
-        @bikes.pop
+        bikes.pop
     end
 
     private
 
+    attr_reader :bikes
+
     def full?
-        @bikes.count >= DEFAULT_CAPACITY
+        bikes.count >= capacity
     end
 
     def empty?
-        @bikes.empty?
+        bikes.empty?
     end
 
 end
